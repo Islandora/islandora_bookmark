ceef106beb135ffccbd34dcc118ac35822ec59ac
Jordan Dukart <jordan@discoverygarden.ca>
2012-11-27 16:47:02 -0400

    Templatable per pid display using the islandora_build_hook_list func as defined in Islandora Core. Output rendered as a table select with 2 or 3 columns depending on shared/owner access. In the format of checkbox, rendered markup, remove button (if owner).

---
+++ theme/islandora_bookmark_default_object_display.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * 
+ * 
+ */
+?>
+
+<?php print l($fedora_object->label, 'islandora/object/' . $variables['fedora_object']->id, array(
+              'attributes' => array(
+                'target' => '_blank'
+                ))); ?>
+
