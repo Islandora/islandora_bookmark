77802b79a94f104681cf944efc6dd86d9b37c756
Jordan Dukart <jordan@discoverygarden.ca>
2012-11-29 18:18:21 -0400

    Code cleanup / movement. Added pagers to both tables, add export functionality to lists. Hooking into Fedora object view to display Bookmark information on pages.

--- theme/islandora_bookmark_default_object_display.tpl.php
+++ theme/islandora_bookmark_default_object_display.tpl.php
@@ -3,6 +3,11 @@
 /**
  * @file
  * 
+ * This is a template for generating the default markup for a Fedora object
+ * in conjunction with Islandora Bookmark.
+ * 
+ * The 'variables' array contains a Fedora Tuque object see 
+ * islandora-object.tpl.php of the core Islandora module for further details.
  * 
  */
 ?>
