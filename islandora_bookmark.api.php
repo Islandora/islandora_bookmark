4e5376e8f56be2e4792ab88dea8421d1ad0b2b82
Jordan Dukart <jordan@discoverygarden.ca>
2012-11-29 18:18:55 -0400

    Add a file to show the hooks for islandora_bookmark.

---
+++ islandora_bookmark.api.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+  * @file
+  * Hooks provided by Islandora Bookmark.
+  */
+
+/**
+  * Hook to collect exports functions available for use.
+  *
+  * @return
+  *   Returns an array with the name of the module and the function name to call
+  *   to handle exportation. In the form of 'module name' => 'export function'.
+  */
+function hook_islandora_bookmark_export_handler() {
+  
+}
+
+/**
+ * Hook to generate markup for displaying a Fedora object within a table.
+ * 
+ * @return
+ *   Returns an array containing the markup for displaying the Fedora object.
+ */
+function hook_islandora_bookmark_object_markup() {
+  
+}
